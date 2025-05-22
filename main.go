27c030bc26fcc836af9232ab630e559052991170
GersonAM-Medismart <gerson.andrade@medismart.live>
2025-05-22 15:20:25 -0400

    prueba variables de entorno

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,6 +13,20 @@ func main() {
 		c.JSON(200, gin.H{
 			"message": "Hello, World!",
 		})
+	})
+
+	router.GET("/env", func(c *gin.Context) {
+		testValue := os.Getenv("TEST_VALUE")
+		if testValue != "" {
+			c.JSON(200, gin.H{
+				"message": "Valor encontrado",
+				"value":   testValue,
+			})
+		} else {
+			c.JSON(404, gin.H{
+				"message": "No se encontró la variable de entorno TEST_VALUE",
+			})
+		}
 	})
 
 	// Inicia el servidor en el puerto 8080
